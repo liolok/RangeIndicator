5ee319bf891543aed938918ff386be88b3db9746
liolok <i@liolok.com>
2025-06-04 13:05:05 +0800

    release

--- modinfo.lua
+++ modinfo.lua
@@ -2,7 +2,7 @@ local function T(en, zh, zht) return ChooseTranslationTable({ en, zh = zh, zht =
 
 name = T('Range Indicator', '范围显示', '範圍顯示')
 author = T('takaoinari, adai1198, (TW)Eric, liolok', 'takaoinari、adai1198、(TW)Eric、李皓奇')
-local date = '2025-04-26'
+local date = '2025-06-04'
 version = date .. '' -- for revision in same day
 description = T(
   'Show ranges by clicking, placing or hovering.',
@@ -12,7 +12,7 @@ description = T(
   'Support binding key at bottom of Settings > Controls page.',
   '支持在设置 > 控制页面底部实时调整键位绑定。',
   '支援在設定 > 控制頁面底部即時調整鍵位綁定。'
-) .. '\n󰀰 ' .. date -- Florid Postern（绚丽之门）
+) .. '\n󰀰 ' .. T('Last updated at: ', '最后更新于：') .. date
 api_version = 10
 dst_compatible = true
 client_only_mod = true
